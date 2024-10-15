88f81d59bf84d97f16e421f8437f6678dfd25b75
Hao Zhang <hzhangxyz@outlook.com>
2024-10-16 03:24:13 +0800

    Add vue dev tools.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 const viteConfig = async () => {
   return {
     base: "./",
-    plugins: [vue()],
+    plugins: [
+      vue({ isProduction: true }),
+      vueDevTools({ launchEditor: "vim" }),
+    ],
   };
 };
 
